7b8729cb9c522ca2d083c99ef37632c7d47b9e2d
AmBro <axmbro@gmail.com>
2024-11-23 19:00:36 +0100

    add utils

---
+++ src/utils/utils.tsx
@@ -0,0 +1,8 @@
+const scrollToElement = (elementId: string) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+export { scrollToElement };
