620318a10194d2ab8e61461f72bb8342de19fc22
AmBro <axmbro@gmail.com>
2024-12-23 22:47:05 +0100

    refactor ProjectsGrid component for improved readability and functionality

--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -1,80 +1,122 @@
 import React from "react";
 import "./ProjectsGrid.css";
 import { useNavigate } from "react-router-dom";
-import starImg from "../assets/star.png"
+import starImg from "../assets/star.png";
+import { Button, ButtonColor } from "./Button.tsx";
+
+interface ProjectItem {
+  title: string;
+  description: string;
+  tags?: string[];
+  imgSrc?: string;
+  logoSrc?: string;
+  star?: boolean;
+  downloadLink?: string;
+  url?: string;
+}
 
 interface ProjectsGridProps {
-  items: { title: string, description: string, tags?: string[], imgSrc?: string, logoSrc?: string, star?: boolean, downloadLink?: string, url?: string }[]
+  items: ProjectItem[];
 }
 
 const ProjectsGrid: React.FC<ProjectsGridProps> = ({ items }) => {
   const navigate = useNavigate();
 
-  const handleProjectClick = (object) => {
-    if (!object.url) {
-      return
+  const handleProjectClick = (project: ProjectItem) => {
+    if (!project.url) {
+      return;
     }
-    navigate(`/projects/${object.url}`);
+    navigate(`/projects/${project.url}`);
   };
 
-  const handleClickDownloadButton = (e) => {
-    e.stopPropagation()
-  }
+  const handleDownloadButtonClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.stopPropagation();
+  };
 
-  return <div className="ProjectsGrid">
-    {items.map((item, index) => {
+  return (
+    <div className="ProjectsGrid">
+      {items.map((item, index) => {
+        const imageSrc = item.imgSrc ? require(`../assets/${item.imgSrc}.png`) : null;
+        const imageAlt = item.imgSrc || "";
 
-      const imageSrc = item.imgSrc ? require(`../assets/${item.imgSrc}.png`) : null;
-      const imageAlt = item.imgSrc ? item.imgSrc : "";
-
-      const renderStar = item.star ? (
-        <div className="StarContainer">
-          <img className="Star" src={starImg} alt="starImg"></img>
-        </div>
-      ) : null;
-
-      const renderLogo = item.logoSrc ? (
-        <img src={require(`../assets/${item.logoSrc}.png`)} alt={item.logoSrc}></img>
-      ) : null
-
-      const renderTags = item.tags ? (
-        <div className="TagsContainer">
-          {
-            item.tags.map((tag, index) => {
-              return <h2 key={`TagKey${index}`} className="Tag">{tag}</h2>
-            })
-          }
-        </div>) : null
-
-      const renderDownloadButton = item.downloadLink ? (
-        <a onClick={handleClickDownloadButton} href={item.downloadLink} className="Button DownloadButton" rel="noopener noreferrer" target="_blank">Download</a>) : null
-
-      return <div key={`ProjectsGridKey${index}`} className="ProjectsGridItem" onClick={() => { handleProjectClick(item) }} style={{ cursor: item.url ? "pointer" : undefined }}>
-        {renderStar}
-        <div className="ImageWrapper">
-          <img src={imageSrc} alt={imageAlt} className="Image"></img>
-        </div>
-        <div className="Container">
-          <div className="Texts">
-            <div className="TitleContainer">
-              <div className="TitleAndLogoContainer">
-                {renderLogo}
-                <h1 className="Title" style={{ color: !item.url ? "var(--primary-text-color)" : undefined }}>{item.title}</h1>
+        return (
+          <div
+            key={`ProjectsGridKey${index}`}
+            className="ProjectsGridItem"
+            onClick={() => handleProjectClick(item)}
+            style={{ cursor: item.url ? "pointer" : undefined }}
+          >
+            {item.star && (
+              <div className="StarContainer">
+                <img className="Star" src={starImg} alt="starImg" />
               </div>
-              <div className="DownloadContainer">
-                {renderDownloadButton}
+            )}
+            {item.imgSrc && (
+              <div className="ImageWrapper">
+                <img src={imageSrc} alt={imageAlt} className="Image" />
               </div>
+            )}
+            <div className="Container">
+              <div className="Texts">
+                <div className="TitleContainer">
+                  <div className="TitleAndLogoContainer">
+                    {item.logoSrc && (
+                      <img
+                        src={require(`../assets/${item.logoSrc}.png`)}
+                        alt={item.logoSrc}
+                      />
+                    )}
+                    <h1
+                      className="Title"
+                      style={{ color: !item.url ? "var(--primary-text-color)" : undefined }}
+                    >
+                      {item.title}
+                    </h1>
+                  </div>
+                  {item.downloadLink && (
+                    <div className="DownloadContainer">
+                      <a
+                        onClick={handleDownloadButtonClick}
+                        href={item.downloadLink}
+                        className="Button DownloadButton"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Button buttonColor={ButtonColor.blue} text="Download" />
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <h2 className="Description">{item.description}</h2>
+              </div>
+              {item.tags && (
+                <div className="TagsContainer">
+                  {item.tags.map((tag, index) => (
+                    <h2 key={`TagKey${index}`} className="Tag">
+                      {tag}
+                    </h2>
+                  ))}
+                </div>
+              )}
             </div>
-            <h2 className="Description">{item.description}</h2>
+            {item.downloadLink && (
+              <div className="DownloadButtonMobile" style={{ marginTop: "1rem" }}>
+                <a
+                  onClick={handleDownloadButtonClick}
+                  href={item.downloadLink}
+                  className="Button DownloadButton"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Button buttonColor={ButtonColor.blue} text="Download" />
+                </a>
+              </div>
+            )}
           </div>
-          {renderTags}
-        </div>
-        <div className="DownloadButtonMobile" style={{ marginTop: renderDownloadButton ? "1rem" : 0 }}>
-          {renderDownloadButton}
-        </div>
-      </div>
-    })}
-  </div>
-}
+        );
+      })}
+    </div>
+  );
+};
 
 export { ProjectsGrid };
