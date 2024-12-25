5bd5c4e884f49bc576efe8729f18e46d8343c4d6
AmBro <axmbro@gmail.com>
2024-12-25 13:27:21 +0100

    refactor App component: integrate ScrollToTop functionality directly and remove unused ScrollToTop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Home from "./pages/Home.tsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import { Header } from "./components/Header.tsx";
 import { Projects } from "./pages/Projects.tsx";
 import Footer from "./components/Footer.tsx";
-import ScrollToTop from "./components/ScrollToTop.tsx";
 import ProjectsItems from "./pages/ProjectsSubpage.tsx";
+
+
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
+    <Router>
+      <div className="App">
         <ScrollToTop />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:projectId" element={<ProjectsItems />} />
         </Routes>
         <Footer></Footer>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
