5514e403ac9442a9da6e0a4563599a3b939fc51c
AmBro <axmbro@gmail.com>
2024-12-25 17:28:48 +0100

    update Projects and One Block Slime Block Adventure pages: refine project tags and enhance descriptions

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,7 +17,7 @@ function Projects() {
                   {
                     title: "Better Bedrock",
                     description: "The Better Bedrock is project of Texture Pack for MCBE Edition, Website and Mobile App available only for Android. The Main goal is to improve the default Minecraft gameplay to a whole new level with many new features!",
-                    tags: ["JSON", "MCBE JsonUI", "React", "Flutter", "Supabase", "Windows API", "Android API"],
+                    tags: [ "MCBE JsonUI", "React", "Flutter", "Supabase", "Windows API", "Android API"],
                     imgSrc: "bbReleaseThumbnail",
                     logoSrc: "bbLogo",
                     star: true,
@@ -27,7 +27,7 @@ function Projects() {
                   {
                     title: "Murder Detector",
                     description: "Simple let us say... shhhh... CHEAT by just TEXTURE PACK. Models system provided by MCBE allows to check what item player is holding, or with additional info, held. Using this user is able to see which person is murderer or sheriff by icon above head or optionally by extra xray!",
-                    tags: ["JSON", "MCBE Models"],
+                    tags: [ "MCBE JsonUI", "MCBE Models"],
                     imgSrc: "murderDetector1",
                     logoSrc: "mdLogo",
                     star: true,
@@ -35,23 +35,32 @@ function Projects() {
                     url: "murder_detector"
                   },
                   {
+                    title: "One Block Slime Block Adventure",
+                    description: "I was part of creating custom HUD elements with custom server from! This was the most advanced project I've worked on, it was a great experience to work with other developers and create something that big and complex!",
+                    tags: [ "MCBE JsonUI", "MCBE Server Form"],
+                    imgSrc: "obsba",
+                    star: true,
+                    downloadLink: "https://www.mushco.games/games",
+                    url: "one_block_slime_block_adventure_page"
+                  },
+                  {
                     title: "Shop UI",
-                    description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom tabs in vanilla style, many texts in each button and search feature! General appearance and in-game UI was fully created by me",
-                    tags: ["JSON", "MCBE JsonUI", "MCBE Server Form"],
+                    description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom tabs in vanilla style, many texts in each button and search feature! General appearance and in-game UI was fully created by me.",
+                    tags: [ "MCBE JsonUI", "MCBE Server Form"],
                     imgSrc: "shop_form1",
                     url: "shop_ui"
                   },
                   {
                     title: "Hometree UI",
                     description: "CUSTOM SERVER FORM UI created for customer. It's pretty colorful and simple grid UI used in gamemodes selector and extra information form! General appearance was designed by customer and slighly by me. In-game UI is fully created by me.",
-                    tags: ["JSON", "MCBE JsonUI", "MCBE Server Form"],
+                    tags: [ "MCBE JsonUI", "MCBE Server Form"],
                     imgSrc: "hometree1",
                     url: "hometree_ui"
                   },
                   {
                     title: "Simple UI",
                     description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom buttons layout! General appearance and in-game UI was fully created by me.",
-                    tags: ["JSON", "MCBE JsonUI", "MCBE Server Form"],
+                    tags: [ "MCBE JsonUI", "MCBE Server Form"],
                     imgSrc: "simple_server_form1",
                     url: "simple_ui"
                   },
@@ -61,15 +70,6 @@ function Projects() {
                     tags: ["HTML", "CSS", "JavaScript", "TypeScript", "React"],
                     imgSrc: "thisweb"
                   },
-                  {
-                    title: "One Block Slime Block Adventure",
-                    description: "I was part of creating custom HUD elements with custom server from! TODO: more info",
-                    tags: ["JSON", "MCBE JsonUI", "MCBE Server Form"],
-                    imgSrc: "obsba",
-                    star: true,
-                    downloadLink: "https://www.mushco.games/games",
-                    url: "one_block_slime_block_adventure_page"
-                  }
                 ]}>
 
               </ProjectsGrid>
