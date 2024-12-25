387637489eeb6a17d8b78c8bd90881240befc81a
AmBro <axmbro@gmail.com>
2024-12-25 13:48:40 +0100

    update Header styles: increase z-index for improved visibility; enhance scrollToElement utility for better offset handling

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,8 +1,21 @@
-const scrollToElement = (elementId: string) => {
-  const element = document.getElementById(elementId);
+const scrollToElement = (id: string, offset = 86) => {
+  const element = document.getElementById(id);
   if (element) {
-    element.scrollIntoView({ behavior: 'smooth', block: "start" });
+    const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
   }
 };
+
+// const scrollToElement = (elementId: string) => {
+//   const element = document.getElementById(elementId);
+//   if (element) {
+//     element.scrollIntoView({ behavior: 'smooth', block: "start" });
+//   }
+// };
 
 export { scrollToElement };
