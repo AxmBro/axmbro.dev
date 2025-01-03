6ead7038fbc535c7c233edd561e66441e1b53451
AmBro <axmbro@gmail.com>
2025-01-03 14:02:52 +0100

    style: remove console log from scrollToElement function for cleaner code

--- src/utils/scroll.tsx
+++ src/utils/scroll.tsx
@@ -2,7 +2,6 @@ const scrollToElement = (id: string) => {
   const element = document.getElementById(id);
   const header = document.getElementById('header');
   const offset = header ? header.offsetHeight : 0;
-  console.log(offset)
 
   if (element) {
     const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
