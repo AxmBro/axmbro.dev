ba171b3e503685bce8c54be1de811480f879c6cf
AmBro <axmbro@gmail.com>
2025-01-03 14:02:58 +0100

    style: enhance ScreenContainer component with dynamic minHeight and scroll behavior on route change

--- src/components/layout/screen_container.tsx
+++ src/components/layout/screen_container.tsx
@@ -1,11 +1,52 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import styles from "./screen_container.module.css";
+import { useLocation } from "react-router-dom";
 
-const ScreenContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
+interface ScreenContainerProps {
+  children: ReactNode,
+  style?: React.CSSProperties
+  useMinHeight?: boolean,
+  id?: string
+}
+
+const ScreenContainer: React.FC<ScreenContainerProps> = ({ children, style, useMinHeight = true, id }) => {
+  const [minHeight, setMinHeight] = useState("0");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "auto",
+    });
+  }, [location]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const header = document.getElementById('header');
+      const headerOffset = header ? header.offsetHeight : 0;
+      const footer = document.getElementById('footer');
+      const footerOffset = footer ? footer.offsetHeight : 0;
+      setMinHeight(
+        useMinHeight ? `calc(100vh - ${headerOffset + footerOffset + 1}px)` : "0"
+      );
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    console.log("xd")
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [useMinHeight]);
+
   return (
-    <div className={styles.screenContainer}>
-      <div className={styles.screenContent}>
-        {children}
+    <div id={id} style={{ minHeight: minHeight }}>
+      <div className={styles.screenContainer} style={style}>
+        <div className={styles.screenContent}>
+          {children}
+        </div>
       </div>
     </div>
   )
