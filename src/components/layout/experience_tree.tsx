c6b52547e2235a59294757e8f7cdf87c0fa15cd7
AmBro <axmbro@gmail.com>
2025-01-03 21:34:02 +0100

    feat: add ExperienceTree and ExperienceTreeContainer components with styling

---
+++ src/components/layout/experience_tree.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode } from "react";
+import styles from "./experience_tree.module.css";
+import { ScreenSectionList } from "./screen_section";
+
+interface ExperienceTreeContainerProps {
+  children?: ReactNode;
+}
+
+const ExperienceTreeContainer: React.FC<ExperienceTreeContainerProps> = ({ children }) => {
+  return (
+    <div className={styles.experienceTreeContainer}>
+      {children}
+    </div>
+  )
+};
+
+interface ExperienceTreeProps {
+  role: string;
+  date: string;
+  company: string;
+  items?: { name: string, value?: string }[];
+  children?: ReactNode;
+}
+
+const ExperienceTree: React.FC<ExperienceTreeProps> = ({ role, date, company, items = [], children }) => {
+  const ScreenSectionListStyle = {
+    padding: 0,
+    border: 0
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.role}>{role}</h1>
+      <h2 className={styles.date}>{date}</h2>
+      <h2 className={styles.company} style={{ color: "var(--secondary-text-color)" }}>{company}</h2>
+      <ScreenSectionList
+        style={ScreenSectionListStyle}
+        items={items}
+      />
+      {children}
+    </div>
+  )
+};
+
+export { ExperienceTree, ExperienceTreeContainer };
