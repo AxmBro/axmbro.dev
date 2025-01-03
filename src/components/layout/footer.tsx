b69a1844ef8b48f208ba4494150c594808467acc
AmBro <axmbro@gmail.com>
2025-01-03 14:01:08 +0100

    style: enhance footer layout with flex adjustments and improved navigation; add responsive design for smaller screens

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,11 +1,67 @@
 import React from "react";
 import styles from "./footer.module.css";
+import { ScreenContainer } from "./screen_container";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { scrollToElement } from "../../utils/scroll";
 
-function Footer() {
+interface FooterProps {
+  openHomeRouteByContactButton: boolean;
+  setOpenHomeRouteByContactButton: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function Footer({ openHomeRouteByContactButton, setOpenHomeRouteByContactButton }: FooterProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigation = (to: string) => {
+    navigate(to);
+  };
+
+
   return (
-    <div className={styles.footer}>
-      <h2>Copyright © AxmBro | All rights reserved</h2>
-    </div>
+    <ScreenContainer
+      id="footer"
+      useMinHeight={false}
+      style={{ backgroundColor: "var(--website-background-color2)", borderTop: "1px solid var(--line-break-color)" }}>
+      <div className={styles.footer}>
+        <div className={styles.routesContainer}>
+          <NavLink
+            to="/"
+            end
+          >
+            <div className={styles.logoContainer}>
+              <img src={require("../../assets/logo192.png")} className={styles.logoImg} alt="" />
+            </div>
+          </NavLink>
+          <NavLink
+            to="/"
+            end
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/projects"
+            end
+          >
+            Projects
+          </NavLink>
+          <h2
+            onClick={() => {
+              if (location.pathname === "/") {
+                scrollToElement("contact");
+              } else {
+                handleNavigation("/");
+                setOpenHomeRouteByContactButton(true);
+              }
+            }}
+          >
+            Contact
+          </h2>
+
+        </div>
+        <h2>Copyright © AxmBro | All rights reserved</h2>
+      </div>
+    </ScreenContainer>
   );
 }
 
