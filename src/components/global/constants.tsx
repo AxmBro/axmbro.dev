d641af5ba3f2f19278ae71a1d102a9012ce84784
AmBro <axmbro@gmail.com>
2025-01-03 21:34:18 +0100

    refactor: replace hardcoded project and skills data with constants

---
+++ src/components/global/constants.tsx
@@ -0,0 +1,115 @@
+export const PROJECTS = [
+  {
+    title: "Better Bedrock",
+    description: "The Better Bedrock is project of Texture Pack for MCBE Edition, Website and Mobile App available only for Android. The Main goal is to improve the default Minecraft gameplay to a whole new level with many new features!",
+    tags: ["MCBE JsonUI", "React", "Flutter", "Supabase", "Windows API", "Android API"],
+    imgSrc: "bbReleaseThumbnail",
+    logoSrc: "bbLogo",
+    star: true,
+    downloadLink: "https://betterbedrock.com/#/downloads",
+    url: "better_bedrock"
+  },
+  {
+    title: "Murder Detector",
+    description: "Simple let us say... shhhh... CHEAT by just TEXTURE PACK. Models system provided by MCBE allows to check what item player is holding, or with additional info, held. Using this user is able to see which person is murderer or sheriff by icon above head or optionally by extra xray!",
+    tags: ["MCBE JsonUI", "MCBE Models"],
+    imgSrc: "murderDetector1",
+    logoSrc: "mdLogo",
+    star: true,
+    downloadLink: "https://betterbedrock.com/#/downloads",
+    url: "murder_detector"
+  },
+  {
+    title: "One Block Slime Block Adventure",
+    description: "I was part of creating custom HUD elements with custom server from! This was the most advanced project I've worked on, it was a great experience to work with other developers and create something that big and complex!",
+    tags: ["MCBE JsonUI", "MCBE Server Form"],
+    imgSrc: "obsba",
+    star: true,
+    downloadLink: "https://www.mushco.games/games",
+    url: "one_block_slime_block_adventure_page"
+  },
+  {
+    title: "Shop UI",
+    description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom tabs in vanilla style, many texts in each button and search feature! General appearance and in-game UI was fully created by me.",
+    tags: ["MCBE JsonUI", "MCBE Server Form"],
+    imgSrc: "shop_form1",
+    url: "shop_ui"
+  },
+  {
+    title: "Hometree UI",
+    description: "CUSTOM SERVER FORM UI created for customer. It's pretty colorful and simple grid UI used in gamemodes selector and extra information form! General appearance was designed by customer and slightly by me. In-game UI is fully created by me.",
+    tags: ["MCBE JsonUI", "MCBE Server Form"],
+    imgSrc: "hometree1",
+    url: "hometree_ui"
+  },
+  {
+    title: "Simple UI",
+    description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom buttons layout! General appearance and in-game UI was fully created by me.",
+    tags: ["MCBE JsonUI", "MCBE Server Form"],
+    imgSrc: "simple_server_form1",
+    url: "simple_ui"
+  },
+  {
+    title: "This Website",
+    description: "Currently created by mainly using JS, TS and React. Simple website with needed info separated by routes. General style is minimalistic and it's in portfolio theme. Previously this website was created using only HTML, JS, CSS - it was pain when implementing routes manually...",
+    tags: ["HTML", "CSS", "JavaScript", "TypeScript", "React"],
+    imgSrc: "thisweb"
+  },
+]
+
+export const EXPERIENCE_TREE = [
+  {
+    role: "Freelancer",
+    date: "NOW",
+    company: "MCBE UI Developer, Web Developer",
+    items: [
+      { name: "Creating custom UI projects. Mainly custom elements for HUD and Server Forms. With my knowledge and experience I am able to create almost everything you want! If you are looking for someone who is able to bring your wildest ideas related to UI, feel free to contact me!" },
+      { name: "Well... to become Web Developer I need more knowledge and experience :D" },
+    ]
+  },
+  {
+    role: "Owner, Lead Developer",
+    date: "07.02.2022 - NOW",
+    company: "Better Bedrock",
+    items: [
+      { name: "Designing and creating new features using JSON in Minecraft Bedrock JsonUI system" },
+      { name: "Creating basic models and simple animations using BlockBench and Minecraft Entity components etc." },
+      { name: "Recording trailers for new version, mostly quick videos, but more recently longer ones called showcase trailers" },
+      { name: "Managing discord comunnity server, in most cases helping and announcing important information" },
+    ]
+  }
+]
+
+export const SKILLS_CARDS = [
+  {
+    title: "Programming",
+    items: [
+      { name: "HTML", value: "7/10" },
+      { name: "CSS", value: "6/10" },
+      { name: "JAVASCRIPT", value: "7/10" },
+      { name: "TYPESCRIPT", value: "4/10" },
+      { name: "Minecraft Bedrock JsonUI", value: "10/10" },
+    ]
+  },
+  {
+    title: "Tools",
+    items: [
+      { name: "Github (Version Control)", value: "6/10" },
+      { name: "Visual Studio Code (IDE)", value: "7/10" }
+    ]
+  },
+  {
+    title: "Learning",
+    items: [
+      { name: "React", value: "5/10" },
+      { name: "Python", value: "3/10" },
+    ]
+  },
+  {
+    title: "Languages",
+    items: [
+      { name: "Polish", value: "Native" },
+      { name: "English", value: "B2, Learning" },
+    ]
+  }
+]
--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -4,6 +4,7 @@ import { Button, ButtonColor } from "../../components/buttons/button";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./projects.module.css"
 import { ScreenContainer } from "../../components/layout/screen_container";
+import { PROJECTS } from "../../components/global/constants";
 
 interface ProjectItem {
   title: string;
@@ -34,64 +35,7 @@ function Projects() {
     localStorage.setItem("hideTags", JSON.stringify(hideTags));
   }, [hideTags]);
 
-  let items: ProjectItem[] = [
-    {
-      title: "Better Bedrock",
-      description: "The Better Bedrock is project of Texture Pack for MCBE Edition, Website and Mobile App available only for Android. The Main goal is to improve the default Minecraft gameplay to a whole new level with many new features!",
-      tags: ["MCBE JsonUI", "React", "Flutter", "Supabase", "Windows API", "Android API"],
-      imgSrc: "bbReleaseThumbnail",
-      logoSrc: "bbLogo",
-      star: true,
-      downloadLink: "https://betterbedrock.com/#/downloads",
-      url: "better_bedrock"
-    },
-    {
-      title: "Murder Detector",
-      description: "Simple let us say... shhhh... CHEAT by just TEXTURE PACK. Models system provided by MCBE allows to check what item player is holding, or with additional info, held. Using this user is able to see which person is murderer or sheriff by icon above head or optionally by extra xray!",
-      tags: ["MCBE JsonUI", "MCBE Models"],
-      imgSrc: "murderDetector1",
-      logoSrc: "mdLogo",
-      star: true,
-      downloadLink: "https://betterbedrock.com/#/downloads",
-      url: "murder_detector"
-    },
-    {
-      title: "One Block Slime Block Adventure",
-      description: "I was part of creating custom HUD elements with custom server from! This was the most advanced project I've worked on, it was a great experience to work with other developers and create something that big and complex!",
-      tags: ["MCBE JsonUI", "MCBE Server Form"],
-      imgSrc: "obsba",
-      star: true,
-      downloadLink: "https://www.mushco.games/games",
-      url: "one_block_slime_block_adventure_page"
-    },
-    {
-      title: "Shop UI",
-      description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom tabs in vanilla style, many texts in each button and search feature! General appearance and in-game UI was fully created by me.",
-      tags: ["MCBE JsonUI", "MCBE Server Form"],
-      imgSrc: "shop_form1",
-      url: "shop_ui"
-    },
-    {
-      title: "Hometree UI",
-      description: "CUSTOM SERVER FORM UI created for customer. It's pretty colorful and simple grid UI used in gamemodes selector and extra information form! General appearance was designed by customer and slightly by me. In-game UI is fully created by me.",
-      tags: ["MCBE JsonUI", "MCBE Server Form"],
-      imgSrc: "hometree1",
-      url: "hometree_ui"
-    },
-    {
-      title: "Simple UI",
-      description: "CUSTOM SERVER FORM UI created for personal use, but also to test custom buttons layout! General appearance and in-game UI was fully created by me.",
-      tags: ["MCBE JsonUI", "MCBE Server Form"],
-      imgSrc: "simple_server_form1",
-      url: "simple_ui"
-    },
-    {
-      title: "This Website",
-      description: "Currently created by mainly using JS, TS and React. Simple website with needed info separated by routes. General style is minimalistic and it's in portfolio theme. Previously this website was created using only HTML, JS, CSS - it was pain when implementing routes manually...",
-      tags: ["HTML", "CSS", "JavaScript", "TypeScript", "React"],
-      imgSrc: "thisweb"
-    },
-  ]
+  let items: ProjectItem[] = PROJECTS
 
   const projectsLength = items.length;
 
