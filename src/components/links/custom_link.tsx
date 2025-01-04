d707234e34c891c8680d0f6fea0b2c611b74d84d
AmBro <axmbro@gmail.com>
2025-01-04 04:08:40 +0100

    feat: enhance Link component to support rendering children instead of text

--- src/components/links/custom_link.tsx
+++ src/components/links/custom_link.tsx
@@ -2,28 +2,46 @@ import React from "react";
 import styles from "./custom_link.module.css";
 
 interface LinkProps {
+  useChildrenInsteadOfText?: boolean,
   useUnderline?: boolean,
   textColor?: string,
   text?: string,
   href?: string,
-  openInNewTab?: boolean
+  openInNewTab?: boolean,
+  children?: React.ReactNode
 }
 
 const Link: React.FC<LinkProps> = ({
+  useChildrenInsteadOfText = false,
   useUnderline = true,
   textColor = "var(--secondary-text-color)",
   text,
   href,
-  openInNewTab = true
+  openInNewTab = true,
+  children
 }) => {
 
-  return <a
-    style={{ color: textColor }}
-    className={useUnderline ? `${styles.link}` : `${styles.linkWithoutUnderline}`}
-    href={href}
-    rel="noopener noreferrer"
-    target={openInNewTab ? "_blank" : undefined}
-  >{text ?? "{text}"}</a>
+  return (
+    <>
+      {!useChildrenInsteadOfText && (<a
+        style={{ color: textColor }}
+        className={useUnderline ? `${styles.link}` : `${styles.linkWithoutUnderline}`}
+        href={href}
+        rel="noopener noreferrer"
+        target={openInNewTab ? "_blank" : undefined}
+      >{text ?? "{text}"}
+      </a>)}
+      {useChildrenInsteadOfText && (
+        <a
+          href={href}
+          rel="noopener noreferrer"
+          target={openInNewTab ? "_blank" : undefined}
+        >
+          {children}
+        </a>
+      )}
+    </>
+  )
 }
 
 export { Link };
