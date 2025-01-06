d913236af40ab816a0ad4fc802c1a084e5750793
AmBro <axmbro@gmail.com>
2025-01-06 01:28:46 +0100

    feat: add NotFound page and route for handling undefined paths

---
+++ src/pages/notFound/notFound.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ScreenSection } from "../../components/layout/screen_section";
+import { ScreenContainer } from "../../components/layout/screen_container";
+import { Button, ButtonColor } from "../../components/button/button";
+import { RouteLink } from "../../components/link/route_link";
+
+const NotFound = () => {
+  return (
+    <ScreenContainer>
+
+      <ScreenSection
+        id="notFound"
+        style={{ padding: "1rem 0 2rem 0", border: 0 }}
+        title="Not found!"
+        description1="Seems like this path is not correct.">
+        <RouteLink
+          useChildrenInsteadOfText={true}
+          to="/">
+          <Button text="Back to Home" buttonColor={ButtonColor.blue}></Button>
+        </RouteLink>
+      </ScreenSection>
+
+    </ScreenContainer>
+  );
+};
+
+export { NotFound };
