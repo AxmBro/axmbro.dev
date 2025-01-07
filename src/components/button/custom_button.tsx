d9bc665fb7fa88726f36055d94e53c7f8cf74792
AmBro <axmbro@gmail.com>
2025-01-08 00:18:50 +0100

    fix: update button border style for consistency and improved clarity

--- src/components/button/custom_button.tsx
+++ src/components/button/custom_button.tsx
@@ -27,11 +27,11 @@ const CustomButton: React.FC<CustomButtonProps> = ({
   const defaultStyles = {
     color: textColor,
     backgroundColor: `rgba(${border.r}, ${border.g}, ${border.b}, 0.1)`,
-    border: `1px var(--border-width) rgba(${border.r}, ${border.g}, ${border.b}, 0.5)`,
+    border: `var(--border-width) solid rgba(${border.r}, ${border.g}, ${border.b}, 0.5)`,
   }
 
   const hoverStyles = {
-    border: `1px var(--border-width) rgba(${border.r}, ${border.g}, ${border.b}, 1)`,
+    border: `var(--border-width) solid rgba(${border.r}, ${border.g}, ${border.b}, 1)`,
   }
 
   return (
