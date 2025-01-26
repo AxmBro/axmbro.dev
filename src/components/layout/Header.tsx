7a466ebe37077fe687abdd10d026958e47ec7bc0
AmBro <axmbro@gmail.com>
2025-01-26 02:38:36 +0100

    refactor: Header component enhancement and update button styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-import { Button } from "../button/Button";
+import { Button, ButtonColor } from "../button/Button";
 import styles from "./Header.module.css";
 import logo from "../../assets/logo192.png";
+import { Link as RouterLink } from "react-router-dom";
 
 const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -21,67 +22,86 @@ const Header: React.FC = () => {
     navigate(to);
   };
 
+  const handleLogoClick = () => {
+    if (location.pathname === "/") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      handleNavigation("/");
+    }
+  }
+
+  const NAV_LINKS = [
+    {
+      to: "/",
+      text: "Home",
+    },
+    {
+      to: "/projects",
+      text: "Projects",
+    },
+    {
+      to: "/contact",
+      text: "Contact",
+    },
+    {
+      to: "/info",
+      text: "Info",
+    },
+  ]
+
   return (
     <div className={styles.headerScreen}>
       <div className={styles.header} id="header">
         <div className={styles.headerContainer}>
+
           <div className={styles.firstSection}>
             <NavLink
-              onClick={() => {
-                if (location.pathname === "/") {
-                  window.scrollTo({
-                    top: 0,
-                    behavior: "smooth",
-                  });
-                } else {
-                  handleNavigation("/");
-                }
-              }}
+              onClick={handleLogoClick}
               to="/"
               end
               style={{ textDecoration: 0 }}
             >
               <div className={styles.logoContainer}>
-                <img src={logo} className={styles.logoImg} alt="" />
-                <h1 className={styles.logo}>Axmbro</h1>
+                <img src={logo} className={styles.logoImg} alt="Axmbro Logo" />
               </div>
             </NavLink>
+            <div className={styles.desktopButtons}>
+              {NAV_LINKS.map((link) => (
+                <NavLink
+                  key={link.to}
+                  to={link.to}
+                  end
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${styles.headerLinkActive}`
+                      : `${styles.headerLink}`
+                  }
+                >
+                  {link.text}
+                </NavLink>
+              ))}
+            </div>
           </div>
 
           <div className={styles.secondSection}>
-            <div className={styles.desktopButtons}>
-              <NavLink
-                to="/"
-                end
-                className={({ isActive }) =>
-                  isActive ? `${styles.headerLinkActive}` : `${styles.headerLink}`
-                }
-              >
-                Home
-              </NavLink>
-              <NavLink
-                to="/projects"
-                end
-                className={({ isActive }) =>
-                  isActive ? `${styles.headerLinkActive}` : `${styles.headerLink}`
-                }
-              >
-                Projects
-              </NavLink>
-              <NavLink
-                to="/contact"
-                end
-                className={({ isActive }) =>
-                  isActive ? `${styles.headerLinkActive}` : `${styles.headerLink}`
-                }
-              >
-                Contact
-              </NavLink>
+            <div className={styles.secondSectionContactButton}>
+              <RouterLink
+                to="/contact" >
+                <Button
+                  buttonColor={ButtonColor.blue}
+                  text="Contact" />
+              </RouterLink>
             </div>
+
+
             <div className={styles.mobileButtons}>
               <div
                 onClick={toggleMenu}
                 style={{ display: menuOpen ? "none" : "inline-block" }}
+                aria-label="Open menu"
               >
                 <h2 className={styles.headerLinkMobile}>Menu</h2>
               </div>
@@ -90,43 +110,25 @@ const Header: React.FC = () => {
                 style={{ display: menuOpen ? "block" : "none" }}
               >
                 <div className={styles.menuContainer}>
-                  <div onClick={toggleMenu}>
-                    <NavLink
-                      to="/"
-                      end
-                      style={{ width: "100%" }}
-                    >
-                      <Button text="Home" style={{ width: "100%", boxSizing: "border-box" }}>
-
-                      </Button>
-                    </NavLink>
-                  </div>
-                  <div onClick={toggleMenu}>
-                    <NavLink
-                      to="/projects"
-                      end
-                      style={{ width: "100%" }}
-                    >
-                      <Button text="Projects" style={{ width: "100%", boxSizing: "border-box" }}></Button>
-                    </NavLink>
-                  </div>
-                  <div onClick={toggleMenu}>
-                    <NavLink
-                      to="/contact"
-                      end
-                      style={{ width: "100%" }}
-                    >
-                      <Button text="Contact" style={{ width: "100%", boxSizing: "border-box" }}></Button>
-                    </NavLink>
-                  </div>
+                  {NAV_LINKS.map((link) => (
+                    <div key={link.to} onClick={toggleMenu}>
+                      <NavLink to={link.to} end style={{ width: "100%" }}>
+                        <Button
+                          text={link.text}
+                          style={{ width: "100%", boxSizing: "border-box" }}
+                        ></Button>
+                      </NavLink>
+                    </div>
+                  ))}
                 </div>
                 <div className={styles.overlay} onClick={closeMenu}></div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 export { Header };
