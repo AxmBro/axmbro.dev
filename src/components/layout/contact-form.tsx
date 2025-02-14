17073adb88bb10121dc08ee8d6690163de5d48e5
AmBro <axmbro@gmail.com>
2025-02-14 14:04:24 +0100

    feat: update contact form styles and improve button functionality

--- src/components/layout/contact-form.tsx
+++ src/components/layout/contact-form.tsx
@@ -1,52 +1,63 @@
 import React, { useState } from "react";
 import styles from "./contact-form.module.css";
 import { Button, ButtonColor } from "../button/Button";
+import global_styles from "./../../components/global/global-styles.module.css";
 
-const ConactForm: React.FC = () => {
+const ContactForm: React.FC = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await fetch("/api/sendEmail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, message }),
-    });
+    try {
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, message }),
+      });
 
-    if (response.ok) {
-      alert("Message sent!");
-      setEmail("");
-      setMessage("");
-    } else {
-      alert("Failed to send message.");
+      if (response.ok) {
+        alert("Message sent!");
+        setEmail("");
+        setMessage("");
+      } else {
+        alert("Failed to send message.");
+      }
+    }
+    catch (error) {
+      console.error(error);
+      alert("Something went wrong. Please try again later.");
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className={styles.contactFormInput}
-        type="email"
-        placeholder="Your Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required />
-      <textarea
-        className={styles.contactFormTextarea}
-        placeholder="Your Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        required>
-      </textarea>
-      <button className={styles.contactFormSubmit} type="submit">
-        <Button text="Submit" style={{ width: "100%" }} buttonColor={ButtonColor.defaultEmpty2}>
-        </Button>
-      </button>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div>
+
+        <p>Email</p>
+        <input
+          className={`${global_styles.formInputText}`}
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required />
+      </div>
+      <div>
+        <p>Message</p>
+        <textarea
+          className={global_styles.formTextArea}
+          placeholder="Enter your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required>
+        </textarea>
+      </div>
+      <Button text="Submit" type="submit" style={{ width: "100%", minHeight: "2.75rem" }} buttonColor={(email && message) ? ButtonColor.default : ButtonColor.defaultEmpty2} />
     </form>
   );
 }
 
-export { ConactForm };
+export { ContactForm };
