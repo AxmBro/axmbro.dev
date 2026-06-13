2eefc9e112f6555416389bdaf8b67d190f704ddf
AxmBro <axmbro@gmail.com>
2026-06-13 18:29:16 +0200

    feat(nav): add hash scroll infrastructure and section anchors

---
+++ public/scripts/hash-scroll-init.js
@@ -0,0 +1,16 @@
+history.scrollRestoration = "manual";
+
+if (location.hash) {
+  var hashParts = location.hash.replace(/^#/, "").split("#").filter(Boolean);
+  var hash = hashParts.length ? "#" + hashParts[0] : "";
+
+  if (hash) {
+    try {
+      sessionStorage.setItem("pending-hash", hash);
+      sessionStorage.setItem("pending-hash-path", location.pathname + location.search);
+    } catch {}
+
+    history.replaceState(null, "", location.pathname + location.search);
+    window.scrollTo(0, 0);
+  }
+}
