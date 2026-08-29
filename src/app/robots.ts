caec3a8de38a043702d8881817389a3cc2b7e4bf
AxmBro <axmbro@gmail.com>
2026-08-30 01:41:57 +0200

    feat(seo): sitemap robots json-ld and metadata helpers

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/api/",
     },
     sitemap: `${SITE_ORIGIN}${SITE_PATHS.sitemapXml}`,
   };
